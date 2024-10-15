43b371dc8bce4ef2540b6edc4fe4332653d32a9a
D.A.Pelasgus <demetrius@pelasgus.xyz>
2024-10-15 20:37:25 +0000

    fork removal

--- scripts/fetch_all_public_repos.sh
+++ scripts/fetch_all_public_repos.sh
@@ -2,10 +2,9 @@
 # Author: D.A.Pelasgus
 #!/bin/bash
 
-# Function to fetch all public repositories owned by the user
+# Function to fetch all public repositories owned by the user, excluding forks
 fetch_all_public_repos() {
   curl -s -H "Authorization: token $GH_TOKEN" \
     "https://api.github.com/users/$GH_USER/repos?type=public&per_page=100" |
-    jq -r '.[] | select(.owner.login == "'$GH_USER'") | .full_name'
+    jq -r '.[] | select(.owner.login == "'$GH_USER'" and .fork == false) | .full_name'
 }
-
