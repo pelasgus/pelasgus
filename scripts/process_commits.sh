f55c840a8368c757ed77dd8098965a26ce1552ed
D.A.Pelasgus <demetrius@pelasgus.xyz>
2024-10-15 20:30:57 +0000

    Update process_commits.sh

--- scripts/process_commits.sh
+++ scripts/process_commits.sh
@@ -1,5 +1,3 @@
-# process_commits.sh
-# Author: D.A.Pelasgus
 #!/bin/bash
 
 # Function to process commits, displaying dropdowns only for repos with merged PRs
@@ -16,10 +14,10 @@ process_commits() {
       # Create a dropdown for repos with merged PRs using HTML formatting
       output+="<details><summary><strong><a href=\"https://github.com/$repo\">$repo</a> - $language</strong></summary>\n\n"
       output+="$(cat "commits_${repo_safe}_${category}.txt")\n\n"
-      output+="</details>\n"
+      output+="</details>\n\n"  # Extra newline for spacing between sections
     else
       # List the repo using Markdown formatting
-      output+="- [$repo](https://github.com/$repo) - $language\n"
+      output+="- [$repo](https://github.com/$repo) - $language\n\n"  # Extra newline for spacing between sections
     fi
   done < owned_repos.txt
 
